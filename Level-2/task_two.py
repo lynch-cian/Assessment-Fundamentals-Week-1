42ab3cfd4c67dda317fe98145f06231b4615d3c0
Cian Lynch <cianlynch345@gmail.com>
2026-02-27 15:09:11 +0000

    Cleaned up task 2, allow Free items to stack also

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -16,9 +16,6 @@ def generate_receipt(basket: list) -> str:
     """Generates a receipt of all items and their prices."""
     basket_output = ""
     total = 0.00
-    item_occurrences = []
-    for item in basket:
-        item_occurrences.append(item)  # for .count(item(Milk)) = 2
     if basket == []:
         return "Basket is empty"
     for item in basket:
@@ -29,10 +26,13 @@ def generate_receipt(basket: list) -> str:
                 basket_output += (
                     f"{item["name"]} x {occurrences} - £{new_price:.2f}\n")
         else:
+            occurrences = basket.count(item)
+            new_price = float(item["price"]) * int(occurrences)
+            if (f"{item["name"]} - Free\n") not in basket_output:
+                basket_output += (
+                    f"{item["name"]} x {occurrences} - Free\n")
             basket_output += (f"{item["name"]} - Free\n")
-
         total += float(item["price"])
-
     basket_output += (f"Total: £{total:.2f}")
     return basket_output
 
